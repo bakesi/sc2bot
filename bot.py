f14adf4dbaea6c92e4ad365769b4aba89bf4de35
Bexultan Zhadyrassynov <beksultan280119@gmail.com>
2018-07-07 00:41:28 +0600

    Attacking the enemy

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, \
 ASSIMILATOR, GATEWAY, CYBERNETICSCORE, STALKER
+import random
 
 def log(message):
     print("BOT: " + message)
@@ -16,6 +17,7 @@ class ProtossBot(sc2.BotAI):
         await self.expand()
         await self.offensive_force_buildings()
         await self.build_offesive_force()
+        await self.attack()
 
     async def build_workers(self):
         for nexus in self.units(NEXUS).ready.noqueue:
@@ -51,13 +53,13 @@ class ProtossBot(sc2.BotAI):
     async def offensive_force_buildings(self):
         if self.units(PYLON).ready.exists:
             pylon = self.units(PYLON).ready.random
-            if self.units(GATEWAY).exists:
-                if not self.units(CYBERNETICSCORE):
-                    if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
-                        log("Bulding cyberneticscore")
-                        await self.build(CYBERNETICSCORE, near=pylon)
-            else:
-                if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
+            if self.units(GATEWAY).exists and not self.units(CYBERNETICSCORE):
+                if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
+                    log("Bulding cyberneticscore")
+                    await self.build(CYBERNETICSCORE, near=pylon)
+            elif self.units(GATEWAY).amount < 3 and \
+                self.can_afford(GATEWAY) and \
+                not self.already_pending(GATEWAY):
                     log("Building gateway")
                     await self.build(GATEWAY, near=pylon)
 
@@ -68,7 +70,28 @@ class ProtossBot(sc2.BotAI):
                     log("Training stalker")
                     await self.do(gw.train(STALKER))
 
+    def find_target(self, state):
+        if len(self.known_enemy_units) > 0:
+            return random.choice(self.known_enemy_units)
+        elif len(self.known_enemy_structures) > 0:
+            return random.choice(self.known_enemy_structures)
+        else:
+            return self.enemy_start_locations[0]
+
+    async def attack(self):
+        if self.units(STALKER).amount > 15:
+            if self.units(STALKER).idle.amount > 0:
+                log("Attack! Get `em!")
+            for s in self.units(STALKER).idle:
+                await self.do(s.attack(self.find_target(self.state)))
+        elif self.units(STALKER).amount > 3:
+            if self.units(STALKER).idle.amount > 0:
+                log("Attack! Get `em!")
+            if len(self.known_enemy_units) > 0:
+                for s in self.units(STALKER):
+                    await  self.do(s.attack(random.choice(self.known_enemy_units)))
+
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, ProtossBot()),
-    Computer(Race.Terran, Difficulty.Easy)
+    Computer(Race.Terran, Difficulty.Medium)
 ], realtime=False)
